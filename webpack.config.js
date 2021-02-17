63338df65c487583ff2f3a118c6df04756f6677f
vbrodar <vedran.brodar@hotmail.com>
2021-02-17 10:41:44 +0100

    Added loaders, hashing and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,60 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
-module.exports = {
+const config = {
   entry: "./app/app.js",
   output: {
-    filename: "myBundle.js",
+    filename: "myBundle.[hash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  mode: "development",
+  devtool: "eval-cheap-source-map",
+  plugins: [new HtmlWebpackPlugin({ template: "./app/index.html" })],
   devServer: {
     port: 8080,
     contentBase: path.resolve(__dirname, "dist"),
     hot: true, //! It works!
   },
-  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                { useBuiltIns: "usage", corejs: 3, targets: "defaults" },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
+
+if (currentTask == "build") {
+  config.mode = "production";
+  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: "main.[hash].css",
+    }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  );
+}
+
+module.exports = config;
